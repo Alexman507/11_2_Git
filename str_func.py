c59958d7ea6819e9986062e84475c3c77291313a
SRE <Nerocross507@gmail.com>
2024-01-22 17:44:35 +0300

    created second function with doc

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def upper(x):
-    """a function that accepts a string as input and returns it with all capital letters"""
+    """a function that accepts a string as input and returns it with all capital letters
+    нууу, не знаю, что сюда добавить =D"""
     return x.upper()
+
+
+def capitalize(x):
+    """Эта функция принимает строку и возвращает его в виде строки с первой заглавной буквы"""
+    return str.capitalize(x)
