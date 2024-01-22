99dac6620b3896d61e582578d148e9701961bb23
Alexman507 <105458434+Alexman507@users.noreply.github.com>
2024-01-22 17:39:22 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,4 @@
 def upper(x):
-    """a function that accepts a string as input and returns it with all capital letters"""
+    """a function that accepts a string as input and returns it with all capital letters
+    добавлю строку, так как не знал, что её надо было в девелопе добавлять =D"""
     return x.upper()
