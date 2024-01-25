45941c1819dd237c1f984017025c32b231748174
SRE <Nerocross507@gmail.com>
2024-01-25 11:35:54 +0300

    Доп функция с повышением регистра

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def upper(x):
     """a function that accepts a string as input and returns it with all capital letters"""
     return x.upper()
+def capitalize(x):
+    """Эта функция принимает строку и возвращает его в виде строки
+     с первой заглавной буквы, только попробуй стереть"""
+    return str.capitalize(x)
