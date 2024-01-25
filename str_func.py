9ce2e37da705399c634607f49905d9844201d509
SRE <Nerocross507@gmail.com>
2024-01-25 11:29:00 +0300

    Revert "created function with doc"
    This reverts commit 97905d40

--- str_func.py
+++ str_func.py
@@ -1,9 +1,3 @@
 def upper(x):
-    """a function that accepts a string as input and returns it with all capital letters
-    нууу, не знаю, что сюда добавить =D"""
+    """a function that accepts a string as input and returns it with all capital letters"""
     return x.upper()
-
-
-def capitalize(x):
-    """Эта функция принимает строку и возвращает его в виде строки с первой заглавной буквы"""
-    return str.capitalize(x)
